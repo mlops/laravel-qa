99b7ad5a5ef3c5ae7a70131964aa74d59a4c0254
mlops <maplops@gmail.com>
2019-05-07 00:21:22 -0300

    Implement from validation and store the qustion and internationalization

---
+++ resources/lang/en/question.php
@@ -0,0 +1,9 @@
+<?php
+
+// resources/lang/en/messages.php
+
+return [
+    'submited' => 'Your Question has been submitted',
+    'success' => 'Success!'
+
+];
---
+++ resources/lang/pt-br/question.php
@@ -0,0 +1,8 @@
+<?php
+
+// resources/lang/en/messages.php
+
+return [
+    'submited' => 'Sua pergunta foi enviada. Aguarde!',
+    'success' => 'Sucesso!'
+];
