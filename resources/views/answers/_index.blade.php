f75ed70c84af9d806b5740b7a8213c839125b246
mlops <maplops@gmail.com>
2019-06-22 14:46:04 -0300

    Implement DRY principle on the application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -7,66 +7,16 @@
                 </div>
                 <hr>
                 @include('layouts._flash-message')
+                
                 @foreach ($answers as $answer)
                 <div class="media">
-                    <div class="d-flex flex-column vote-controls">
-                        {{-- <a href="#" title="This answer is useful" class="vote-up">
-                            <i class="fas fa-caret-up fa-3x"></i>
-                        </a>
-                        <span class="votes-count">1230</span>
-                        <a href="#" title="This answer is no useful" class="vote-down off">
-                            <i class="fas fa-caret-down fa-3x"></i>
-                        </a> --}}
-                        <a href="#" title="This answer is useful" class="vote-up-{{Auth::guest() ? 'favorited' : '' }}" onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit()">
-                            <i class="fas fa-caret-up fa-3x"></i>
-                        </a>
-                        <form action="/answers/{{ $answer->id }}/vote" method="POST" id="up-vote-answer-{{ $answer->id }}" style="display:none">
-                            @csrf
-                            <input type="hidden" name="vote" value="1">
-                        </form>
+                   
                         
-                        <span class="votes-count">{{ $answer->votes_count}}</span>
-                        
-                        <a href="#" title="This question is no useful" class="vote-down {{Auth::guest() ? 'favorited' : '' }}" onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit()">
-                            <i class="fas fa-caret-down fa-3x"></i>
-                        </a>
-                        <form action="/answers/{{ $answer->id }}/vote" method="POST" id="down-vote-answer-{{ $answer->id }}" style="display:none">
-                            @csrf
-                            <input type="hidden" name="vote" value="-1">
-                        </form>
-                        
-                        {{-- <a href="#" title="Click to mark as favorite question(Click again to undo)" class="favorite mt-2 {{ Auth::guest() ? 'off' : ($answer->is_favorited ? 'favorited' : '') }}" onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $answer->id }}').submit()">
-                        
-                            <i class="fas fa-star fa-2x"></i>
-                            <span class="favorites-count">{{ $answer->favorites_count}}</span>
-                        </a>
-                        <form action="/answers/{{ $answer->id }}/favorites" method="POST" id="favorite-question-{{ $answer->id }}" style="display:none">
-                            @csrf
-                            @if($answer->is_favorited)
-                            @method ('DELETE')
-                            @endif
-                        
-                        </form> --}}
-                        @can('accept',$answer)
+                        @include('share._vote', [
+                        'model' => $answer
+                        ])
+
                     
-                            <a title="Mark this answer as best answer" class="{{ $answer->status}} mt-2"
-                            onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit()">
-                                <i class="fas fa-check fa-2x"></i>
-                            </a>
-                        <form action="{{route('answers.accept', $answer->id) }}" method="POST" id="accept-answer-{{ $answer->id }}" style="display:none">
-                            @csrf
-
-                        </form>
-                    @else
-                        @if ($answer->is_best)
-                            <a title="The question owner accepted this answer as best answer" class="{{ $answer->status}} mt-2" >
-                                <i class="fas fa-check fa-2x"></i>
-                            </a>
-                            
-                        @endif
-                    @endcan
-
-                    </div>
                     <div class="media-body">
                         {!! $answer->body_html !!}
                         <div class="row">
@@ -88,15 +38,10 @@
                             </div>
                             <div class="col-4"></div>
                             <div class="col-4">
-                                <span class="text-muted">Answered {{ $answer->created_date}}</span>
-                                <div class="media mt-2">
-                                    <a href="{{ $answer->user->url}}" class="pr-2">
-                                        <img src="{{ $answer->user->avatar}}"></a>
-                            
-                                    <div class="media-body mt-1">
-                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
-                                    </div>
-                                </div>
+                               @include('share._author', [ 
+                                   'model' => $answer,
+                                   'label' => 'answered'
+                               ])
                             </div>
                         </div>
                        
---
+++ resources/views/share/_vote.blade.php
@@ -0,0 +1,49 @@
+@if($model instanceof App\Question)
+@php
+$name = 'question';
+$firstURISegment = 'questions';
+
+@endphp
+@elseif($model instanceof App\Answer)
+@php
+$name = 'answer';
+$firstURISegment = 'answers';
+
+@endphp
+@endif
+
+@php
+    $formId = $name ."-" . $model->id;
+    $formAction = "/{$firstURISegment}/{$model->id}/vote";
+@endphp
+
+<div class="d-flex flex-column vote-controls">
+    <a href="#" title="This {{$name}} is useful" class="vote-up-{{Auth::guest() ? 'favorited' : '' }}" onclick="event.preventDefault(); document.getElementById('up-vote-{{$formId }}').submit()">
+        <i class="fas fa-caret-up fa-3x"></i>
+    </a>
+    <form action="{{$formAction}}" method="POST" id="up-vote-{{$formId }}" style="display:none">
+        @csrf
+        <input type="hidden" name="vote" value="1">
+    </form>
+
+    <span class="votes-count">{{ $model->votes_count}}</span>
+
+    <a href="#" title="This {{$name}} is no useful" class="vote-down {{Auth::guest() ? 'favorited' : '' }}" onclick="event.preventDefault(); document.getElementById('down-vote-{{$formId }}').submit()">
+        <i class="fas fa-caret-down fa-3x"></i>
+    </a>
+    <form action="{{$formAction}}" method="POST" id="down-vote-{{$formId }}" style="display:none">
+        @csrf
+        <input type="hidden" name="vote" value="-1">
+    </form>
+
+    @if ($model instanceof App\Question)
+        @include('share._favorite', [
+            'model' => $model
+        ])
+    @elseif($model instanceof App\Answer)
+        @include('share._accept', [
+        'model' => $model
+        ])
+    @endif
+
+</div>
